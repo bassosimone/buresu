d1e560006c61270bd0c29d4ac7f26b9aa2fc7424
Simone Basso <bassosimone@gmail.com>
2024-11-03 11:24:35 +0100

    cleanup: remove legacy code
    At this point, the code is not useful as a reference and it's
    just a dead whale that we're carrying over. Let's zap it.

--- pkg/legacy/runtime/doc.go
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-// Package runtime provides the core data structures and interfaces for the
-// interpreter's runtime environment. It includes definitions for various
-// value types (e.g., boolean, integer, float, string, unit) and the
-// Value interface that all runtime values must implement. The package
-// also defines the Callable interface for functions and lambdas, as well
-// as built-in functions that can be used within the interpreter.
-package runtime
