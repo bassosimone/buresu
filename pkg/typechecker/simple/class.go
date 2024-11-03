e7c0b7839648978d1a55c214ff0336f2e754caba
Simone Basso <bassosimone@gmail.com>
2024-11-03 11:39:51 +0100

    cleanup(typechecker/simple): remove unused class.go

--- pkg/typechecker/simple/class.go
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-package simple
-
-// Class is a class to which types belong.
-type Class interface {
-	// Instantiate instantiates the type class in the given environment.
-	Instantiate(env *Environment)
-}
